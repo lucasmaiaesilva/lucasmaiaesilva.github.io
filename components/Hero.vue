ea3540895045743bea628461d73d9ba35d961f43
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2018-02-22 11:02:49 -0300

    Feat: Remove readme on dist version

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,8 +1,10 @@
 <template>
   <section class="hero">
     <div class="container">
-      <div class="hero-content">
-        conteúdo do Hero Maroto
+      <div class="hero-container">
+        <div class="hero-content">
+          conteúdo do Hero Maroto
+        </div>
       </div>
     </div>
   </section>
@@ -10,24 +12,14 @@
 
 <style>
   .hero {
-    background: url("~/assets/img/background.png");
-    background-size: cover;
+    /* background: url("~/assets/img/background.png");
+    background-size: cover; */
+    background: #333644;
   }
-
-  .hero-content {
-    background: #fff url("~/assets/img/browser.png") no-repeat 50% 0;
-    padding-top: 3.5rem;
-    padding-left: 1rem;
-    background-size: 100%;
-    color: #d3d3d3;
-    margin: 3rem auto;
-    border-radius: 8px;
-    height: 19rem;
-    width: 90%;
-  }
-  @media screen and (min-width: 734px) {
-    .hero-content {
-      padding-top: 5rem;  
-    }
+  .hero .hero-container {
+    box-shadow: inset 0 0 1em gold, 0 0 1em gold;
+    border: 3px solid #fff;
+    border-radius: 3px;
+    margin: 1rem 0;
   }
 </style>
